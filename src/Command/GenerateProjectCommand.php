3123857683d6dcab40bf70405916ee8ab67aa6b8
Edouard Cunibil <edouard@happyculture.coop>
2020-07-23 15:48:00 +0200

    Reuse data in interactive mode when possible.

--- src/Command/GenerateProjectCommand.php
+++ src/Command/GenerateProjectCommand.php
@@ -141,6 +141,8 @@ class GenerateProjectCommand extends Command {
    * {@inheritdoc}
    */
   protected function interact(InputInterface $input, OutputInterface $output) {
+    $envVars = getenv();
+
     // Identify the Drupal version built.
     try {
       $core_version = $input->getOption('core') ? $input->getOption('core') : null;
@@ -173,7 +175,7 @@ class GenerateProjectCommand extends Command {
     if (!$name) {
       $name = $this->getIo()->ask(
         'What is the human readable name of the project?',
-        'Happy Rocket',
+        array_key_exists('COMBAWA_PROJECT_NAME', $envVars) ? $envVars['COMBAWA_PROJECT_NAME'] : 'Happy Rocket',
         function ($name) {
           return $this->validateName($name);
         }
@@ -192,7 +194,7 @@ class GenerateProjectCommand extends Command {
     if (!$machine_name) {
       $machine_name = $this->getIo()->ask(
         'What is the machine name of the project?',
-        $this->stringConverter->createMachineName($name),
+        array_key_exists('COMBAWA_PROJECT_MACHINE_NAME', $envVars) ? $envVars['COMBAWA_PROJECT_MACHINE_NAME'] : $this->stringConverter->createMachineName($name),
         function ($machine_name) {
           return $this->validateMachineName($machine_name);
         }
